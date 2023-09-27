3b92a1299600a119dfbf5873e3493616b6a183fa
felipejoq <felipejofreq@gmail.com>
2023-09-27 20:55:04 -0300

    Cambio de diseño del badge en el card

--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -2,7 +2,7 @@ import Badge from "react-bootstrap/Badge";
 
 const Tag = ({text, color}) => {
     return (
-        <Badge className={"w-100 m-auto"} bg={color}>{text}</Badge>
+        <Badge bg={color}>{text}</Badge>
     );
 };
 
